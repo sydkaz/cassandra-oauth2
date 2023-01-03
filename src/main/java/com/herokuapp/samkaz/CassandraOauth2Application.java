4ddbba59671619e76017ed76b59d8d7893695962
Qumber <qumberkazmi@gmail.com>
2023-01-03 13:10:47 +0500

    Initial commit

---
+++ src/main/java/com/herokuapp/samkaz/CassandraOauth2Application.java
@@ -0,0 +1,13 @@
+package com.herokuapp.samkaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/herokuapp/samkaz/CassandraOauth2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.herokuapp.samkaz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+class CassandraOauth2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
